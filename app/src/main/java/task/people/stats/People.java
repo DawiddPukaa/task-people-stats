8b0bcb13ae5d1ba14e50177424e87f36c7b382da
DawiddPukaa <dawidpuka@gmail.com>
2020-11-09 21:27:30 +0100

    second commit

--- app/src/main/java/task/people/stats/People.java
+++ app/src/main/java/task/people/stats/People.java
@@ -3,9 +3,15 @@ package task.people.stats
 
 class Person {
     private final String firstName;
+    private final Strong lastname;
 
-    public Person(String firstName) {
+    public Person(String firstName, Strong lastname) {
         this.firstName = firstName;
+        this.lastname = lastname;
+    }
+
+    public Strong getLastname() {
+        return lastname;
     }
 
     public String getFirstName() {
