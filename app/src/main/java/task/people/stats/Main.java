e2830881bcd67d4b7618a885f78020a6bd764891
DawiddPukaa <dawidpuka@gmail.com>
2020-11-09 21:36:45 +0100

    fiveth commit

--- app/src/main/java/task/people/stats/Main.java
+++ app/src/main/java/task/people/stats/Main.java
@@ -4,7 +4,8 @@ package task.people.stats
 class Main {
     public static void main(String[] args) {
         PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
-
+        System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+        System.out.println(String.format("Liczba osób z unikalnymi nazwiskami: %d", peopleStats.countUniqueLastNames()));
         System.out.println("Hello world");
     }
 }
