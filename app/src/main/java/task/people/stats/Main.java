af0be9d0769b525ec1bca43017cd372d5d2b2327
DawiddPukaa <dawidpuka@gmail.com>
2020-11-09 21:33:59 +0100

    fourth commit

--- app/src/main/java/task/people/stats/Main.java
+++ app/src/main/java/task/people/stats/Main.java
@@ -3,6 +3,8 @@ package task.people.stats
 
 class Main {
     public static void main(String[] args) {
+        PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
         System.out.println("Hello world");
     }
 }
